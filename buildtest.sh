2c96dba2ecb6b77b8285ae70800aeffd92e60e6a
Bjarturfannar <bjartur15@ru.is>
2016-12-06 15:12:15 +0000

    minor fix

--- buildtest.sh
+++ buildtest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Clean the build repo and rebuild"
-npm run build
-
 echo "installing npm for jenkins"
 npm install
 cd client
 npm install
 cd ..
+
+echo "Clean the build repo and rebuild"
+npm run build
 
 echo "Copy Dockerfile, package.json to build and copy run.sh to build Dockerfile"
 cp ./Dockerfile ./build/
